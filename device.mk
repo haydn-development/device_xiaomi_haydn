4d278f7c3b5c7ca0ed2cbf1c16002f247b7e11e8
sadoyui <wiliamweiyi@gmail.com>
2023-08-18 23:46:30 +0000

    haydn: fingerprint: Reboard to haydn

--- device.mk
+++ device.mk
@@ -185,7 +185,7 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_sm8350
+    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_haydn
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
